46170f3270fb5be6e60bfe2b45b627bca620504b
srFelipes <37315690+srFelipes@users.noreply.github.com>
2020-03-25 13:07:14 -0300

    Update spitest.py
    Se añaden comentarios

--- src_firmware/spitest.py
+++ src_firmware/spitest.py
@@ -1,5 +1,26 @@
 #!/usr/bin/python
 
+
+#Este script tiene las dos funciones que se comunican al encoder.
+
+# leer(spi,cs) entrega la posicion absoluta del encoder asociado al gpio (cs) cs={cs_hombro,cs_codo,cs_z}
+
+#abs2offset(spi,cs,dato) llama a leer(...) para calcular el offset en base a la lectura del encoder en cs. Dato es una tupla [angulo,offset].
+#La variable asociada al offset es odrv0.axis0.encoder.config.offset.
+
+#Procedimiento:     (de preferencia tener el motor con lectura de encoder en 180° o ponerlo en una posicion donde pueda moverse a ambos lados y asociar este valor a 180 por spi
+#encender odrive 
+#leer posicion angular del encoder Ej: In [1]: dato[0]=leer(spi,cs_z)         
+#Realizar secuencia de calibración enconder Ej: In [2]: odrv0.axis0.requested_state = AXIS_STATE_ENCODER_OFFSET_CALIBRATION 
+#Copiar offset obtenido a dato Ej: In [1]: dato[1]=odrv0.axis0.encoder.config.offset
+#Guardar dato de forma permanente 
+
+#En una nueva inicialización
+ #cargar dato (Quizas guardar en spitest (?))
+# Actualizar offset según abs2offset  Ej: In[1]: odrv0.axis0.encoder.config.offset=abs2offset(spi,cs_z,dato)
+#setear encoder a listo Ej: In[1]: odrv0.axis0.encoder.is_ready=true
+#done!
+
 import spidev
 import time
 import RPi.GPIO as GPIO
@@ -49,8 +70,8 @@ def leer(spi,cs):
 
 def abs2offset(spi,cs,dato):
     current=leer(spi,cs)
-    if abs(current-dato[0])>180:
-        current = current +360 
+    #if abs(current-dato[0])>180:
+    #    current = current +360 
         
     return dato[1]+(current-dato[0])/360.0*4000
         
