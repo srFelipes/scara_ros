23430b6a61e8a5d22cf836e956a0496e7a3fb029
srFelipes <37315690+srFelipes@users.noreply.github.com>
2020-04-16 12:06:38 -0400

    Función run_once
    Se añade función run_once(p,i,d,joint)
    que corre una simulación con parametro p,i,d ,joint
    joint puede ser:
    'hombro'
    'codo'
    'z'

--- nelen_control/python/nodo_pso.py
+++ nelen_control/python/nodo_pso.py
@@ -1,77 +1,87 @@
 #!/usr/bin/env python
 import time
-
-real_init=time.time()
+# Se importan paquetes importantes
 import math
 import rospy
 import numpy as np
-
-
 
 # import Float64 msg
 from std_msgs.msg import Float64
 # import control msg
 from control_msgs.msg import JointControllerState
+
+#necesario para llamar a los servicions de pausa despausa y reinicializacion
 from std_srvs.srv import Empty
 
+#paquete para cambiar los parametros del PID
 import dynamic_reconfigure.client
 
-print('inicializar funciones')
-t_init=time.time()
+#se crean funciones para operar gazebo
 reset_simulation = rospy.ServiceProxy('/gazebo/reset_simulation', Empty)
 pause_simulation = rospy.ServiceProxy('/gazebo/pause_physics', Empty)
 unpause_simulation = rospy.ServiceProxy('/gazebo/unpause_physics', Empty)
-print(time.time()-t_init)
 
+#variables universales para guardar los datos de la realziacion
 data=[]
 tiempo=[]
-estoyen0=False
 
 
-def codo_callback(msg):
-    #print("  angle: %f " % msg.process_value)
-    #print("    set_point: %f " % msg.set_point)
+#esta funcion se opera cada vez que el nodo recibe un mensaje
 
+def listener_callback(msg):
+    #print('guardardato')
+
+    #se guarda el error
     data.append(msg.error)
-    #print(type(msg.error))
+    #se guarda el tiempo
     tiempo.append(msg.header.stamp.to_sec())
-    #print(type(msg.header.stamp.to_sec()))
-    # here we can do more stuff
-    # blabla
 
-def main_loop():
+def run_once(p,i,d,joint):
+    T_abs=time.time()
     rospy.init_node('run_simulation_once')
-    print(rospy.get_time())
+
     condition= False
     t_escalon=0.5
     t_it=2.0
-    # Create publisher for codo
+    # suscribirse a los joints segun sea el caso
+    if joint=='codo':
+        #setear codo
+        client = dynamic_reconfigure.client.Client('/nelen/codo_cont/pid')
+        pub_codo_cmd = rospy.Publisher('/nelen/codo_cont/command', Float64, queue_size=10)
+        sub_codo_state = rospy.Subscriber('/nelen/codo_cont/state', JointControllerState, listener_callback)
 
-    client = dynamic_reconfigure.client.Client('/nelen/hombro_control/pid')
+    elif joint=='hombro':
+        #setear hombro
+        client = dynamic_reconfigure.client.Client('/nelen/hombro_control/pid')
+        pub_codo_cmd = rospy.Publisher('/nelen/hombro_control/command', Float64, queue_size=10)
+        sub_codo_state = rospy.Subscriber('/nelen/hombro_control/state', JointControllerState, listener_callback)
 
-    pub_codo_cmd = rospy.Publisher('/nelen/codo_cont/command', Float64, queue_size=10)
-    sub_codo_state = rospy.Subscriber('/nelen/codo_cont/state', JointControllerState, codo_callback)
+    elif joint=='z':
+        #setear z
+        client = dynamic_reconfigure.client.Client('/nelen/prism_control/pid')
+        pub_codo_cmd = rospy.Publisher('/nelen/prism_control/command', Float64, queue_size=10)
+        sub_codo_state = rospy.Subscriber('/nelen/prism_control/state', JointControllerState, listener_callback)
+
+    else:
+        print('not a joint')
+
+    params={'p':p,'i':i,'d':d}
+    config = client.update_configuration(params)
     # Set publication rate
-    rate = rospy.Rate(100) # 100hz
+    rate = rospy.Rate(50) # 100hz
     pub_codo_cmd.publish(0.0)
 
-    print('tiempo reset')
-    t_init=time.time()
     reset_simulation()
-    print(time.time()-t_init)
     # Create a subscriber for codo
 
-    print('tiempo despausa')
-    t_init=time.time()
     unpause_simulation()
-    print(time.time()-t_init)
 
 
     # Main loop
     # We'll publish commands at the rate defined before
 
 
-
+    print(time.time()-T_abs)
     t_init=rospy.get_time()
     while not condition:
         t = rospy.get_time() # get current time
@@ -84,11 +94,15 @@ def main_loop():
             print(len(data))
             condition = True
             pub_codo_cmd.publish(0.0)
-            params={'p':80.0}
-            config = client.update_configuration(params)
-            print(rospy.get_time()-t_init)
-            print('finalmente')
-            print(real_init-time.time())
+
+            #elevar al cuadrado
+            data2=[thisData**2 for thisData in data]
+            #se remueve el primer dato
+            data2.remove(data2[0])
+
+            #calcular la diferencia de tiempo por el error al cuadrado
+            ponderados=[thisTiempo*thisData for thisTiempo,thisData in zip(np.diff(tiempo),data2)]
+            print(sum(ponderados))
 
             break
 
@@ -108,6 +122,6 @@ def main_loop():
 
 if __name__ == '__main__':
     try:
-        main_loop()
+        run_once(100.0,2.0,3.0,'hombro')
     except rospy.ROSInterruptException:
         pass
