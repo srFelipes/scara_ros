b18826a64cdd9e87736a6be854239f2d4a571dc1
srFelipes <37315690+srFelipes@users.noreply.github.com>
2020-04-16 19:31:30 -0400

    Integracion nodos pso
    Se integra el codigo del wiss con el mio

--- nelen_control/python/nodo_pso.py
+++ nelen_control/python/nodo_pso.py
@@ -4,6 +4,8 @@ import time
 import math
 import rospy
 import numpy as np
+from tqdm import tqdm
+import copy
 
 # import Float64 msg
 from std_msgs.msg import Float64
@@ -25,9 +27,87 @@ unpause_simulation = rospy.ServiceProxy('/gazebo/unpause_physics', Empty)
 data=[]
 tiempo=[]
 
+#
+class Particle:
+    def __init__(self, x, c1, c2):
+        self.dim = len(x)
+        self.pos_i = x
+        self.vel_i = np.random.uniform(-1, 1, self.dim)
+        self.pos_best_i = []
+        self.err_best_i = 1
+        self.err_i = 1
+        self.c1 = c1
+        self.c2 = c2
+
+    def evaluate(self, costFunc):
+        self.err_i = costFunc(self.pos_i) # evaluate parameter's performance
+
+        # update pos_best_i and err_best_i
+        if self.err_i < self.err_best_i:
+            self.pos_best_i = self.pos_i
+            self.err_best_i = self.err_i
+
+    def update_vel(self, pos_best_g, w):
+        r1 = np.random.random(self.dim)
+        r2 = np.random.random(self.dim)
+        vel_cognitive = self.c1 * r1 * (np.array(self.pos_best_i) - np.array(self.pos_i))
+        vel_social = self.c2 * r2 * (np.array(pos_best_g) - np.array(self.pos_i))
+        self.vel_i = w * self.vel_i + vel_cognitive + vel_social
+
+    def update_pos(self, bounds):
+        self.pos_i += self.vel_i
+
+        for i in range(self.dim):
+            if self.pos_i[i] > bounds[i][1]:
+                self.pos_i[i] = bounds[i][1]
+            if self.pos_i[i] < bounds[i][0]:
+                self.pos_i[i] = bounds[i][0]
+class PSO():
+    def __init__(self, costFunc, bounds, N, iter, c1, c2, pos_best_g=[], err_best_g=1):
+        self.err_log = np.zeros((N, iter))
+        xdim = len(bounds)
+
+        self.pos_best_g = pos_best_g
+
+        self.err_best_g = err_best_g
+
+        swarm = []
+        for i in range(N):
+            xx = []
+            for d in range(xdim):
+                xx.append(np.random.uniform(bounds[d][0], bounds[d][1]))
+            swarm.append(Particle(xx, c1, c2))
+
+        print("Birds are exploring...")
+        i = 0
+
+        pbar = tqdm(total=iter * N)
+        while i < iter:
+            w = np.exp(-i / iter)
+
+            for j in range(N):
+                swarm[j].evaluate(costFunc)
+                self.err_log[j, i] = swarm[j].err_i
+
+                if swarm[j].err_i < self.err_best_g:
+                    self.pos_best_g = copy.deepcopy(swarm[j].pos_i)
+                    self.err_best_g = copy.deepcopy(swarm[j].err_i)
+                pbar.update(1)
+            print("Best position so far : ", self.pos_best_g)
+            print("Best error so far : ", self.err_best_g)
+
+            for j in range(N):
+                swarm[j].update_vel(self.pos_best_g, w)
+                swarm[j].update_pos(bounds)
+
+            i += 1
+        pbar.close()
+
+        print("Iteration :", iter)
+        print("Optimized parameters :", self.pos_best_g)
+        print("Minimum error :", self.err_best_g)
 
 #esta funcion se opera cada vez que el nodo recibe un mensaje
-
 def listener_callback(msg):
     #print('guardardato')
 
@@ -90,8 +170,6 @@ def run_once(p,i,d,joint):
         # publish command
         if t>=t_it+t_init:
             pause_simulation()
-            print(len(tiempo))
-            print(len(data))
             condition = True
             pub_codo_cmd.publish(0.0)
 
@@ -102,9 +180,7 @@ def run_once(p,i,d,joint):
 
             #calcular la diferencia de tiempo por el error al cuadrado
             ponderados=[thisTiempo*thisData for thisTiempo,thisData in zip(np.diff(tiempo),data2)]
-            print(sum(ponderados))
-
-            break
+            return (sum(ponderados))
 
         elif t>=t_escalon+t_init:
             pub_codo_cmd.publish(1.0)
@@ -120,8 +196,17 @@ def run_once(p,i,d,joint):
     # If we had, the callback will be called and the state of the codo will be printed
     #rospy.spin()
 
+def costFunc(x):
+  # simulate with Gazebo with parameters x=[p,i,d]
+  jointAentrenar='hombro'
+  return run_once(x[0],x[1],x[2],jointAentrenar)
+
 if __name__ == '__main__':
     try:
-        run_once(100.0,2.0,3.0,'hombro')
+        iter_num = 3 # number of iterations
+        particle_num = 2 # particle size
+        bounds_params = [[1,50],[1,50],[1,50]]
+        pso=PSO(costFunc=costFunc,pos_best_g=[],err_best_g=1,bounds=bounds_params,N=particle_num,iter=iter_num,c1=1,c2=2)
+        print(pso.pos_best_g)
     except rospy.ROSInterruptException:
         pass
